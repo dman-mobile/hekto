e95e8dce2e5ccdc9fb458ea15343a82fd1219a62
wasyted <mataiasa.d@gmail.com>
2025-09-12 12:10:45 -0300

    feat(HEK#7): mueve componentes a styles.tsx

--- src/components/Trending.styles.tsx
+++ src/components/Trending.styles.tsx
@@ -1,4 +1,6 @@
-import { FlexCenter } from "../styled-components/Flex.styles"
+import { Section } from "../styled-components/Section.styles"
+import { Label } from "../styled-components/Typography.styles"
+import { FlexCenter, FlexColumn } from "../styled-components/Flex.styles"
 import { styled } from "@mui/system"
 
 export const PriceDiscount = styled('p')({
@@ -8,4 +10,48 @@ export const PriceDiscount = styled('p')({
 
 export const PriceContainer = styled(FlexCenter)({
   gap: 16
+})
+
+export const TrendingSection = styled(Section)({
+})
+
+export const Card = styled(FlexColumn)({
+  padding: 16,
+  boxShadow: 'var(--shadow)',
+  transition: 'all .3s ease-in-out',
+  ':hover': {
+    boxShadow: 'var(--shadow-large)',
+    transform: 'translateY(-25px)',
+  },
+  width: '100%',
+  alignItems: "center"
+})
+
+export const CardBody = styled(FlexColumn)({
+  padding: 24,
+  alignItems: 'center'
+})
+
+export const ProductTitle = styled(Label)({
+  color: 'var(--color-primary)',
+  textAlign: 'center',
+  textTransform: 'capitalize'
+})
+
+export const ProductsWrapper = styled(FlexCenter)({
+  gap: 'var(--spacing-unit)',
+  maxWidth: 1280,
+  margin: '0 auto',
+  marginTop: 72
+})
+
+export const ImageWrapper = styled('div')({
+  height: 232,
+  width: 272,
+  '& img': {
+    height: '100%',
+    width: '100%',
+    objectFit: 'cover'
+  },
+  overflow: 'hidden'
 })
