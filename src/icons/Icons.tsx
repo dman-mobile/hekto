7dcfe66bfad990c7eaed2ba39bdcb36888d23f4c
wasyted <mataiasa.d@gmail.com>
2025-09-17 11:20:28 -0300

    feat(HEK#17): arregla conflictos

--- src/icons/Icons.tsx
+++ src/icons/Icons.tsx
@@ -14,7 +14,7 @@ export const ZoomIcon = () => (
   <svg fill="none" height="16" viewBox="0 0 16 16" width="16" xmlns="http://www.w3.org/2000/svg">
     <use href="/icons.svg#icon-zoom"></use>
   </svg>
-)
+);
 
 export const ChevronLeftIcon = () => (
   <svg fill="none" height="16" viewBox="0 0 16 16" width="16" xmlns="http://www.w3.org/2000/svg">
@@ -32,75 +32,76 @@ export const TickIcon = () => (
   <svg fill="none" height="16" viewBox="0 0 16 16" width="16" xmlns="http://www.w3.org/2000/svg">
     <use href="/icons.svg#icon-tick"></use>
   </svg>
-)
+);
 
 export const PenIcon = () => (
   <svg fill="none" height="16" viewBox="0 0 16 16" width="16" xmlns="http://www.w3.org/2000/svg">
     <use href="/icons.svg#icon-pen"></use>
   </svg>
-)
+);
 
 export const CalendarIcon = () => (
   <svg fill="none" height="16" viewBox="0 0 16 16" width="16" xmlns="http://www.w3.org/2000/svg">
     <use href="/icons.svg#icon-calendar"></use>
   </svg>
-)
+);
 
 export const GridIcon = () => (
   <svg fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
     <use href="/icons.svg#icon-view-grid"></use>
   </svg>
-)
+);
 
 export const ListIcon = () => (
   <svg fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
     <use href="/icons.svg#icon-view-list"></use>
   </svg>
-)
+);
 
 export const GridIconFilled = () => (
   <svg fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
     <use href="/icons.svg#icon-view-grid-filled"></use>
   </svg>
-)
+);
 
 export const ListIconFilled = () => (
   <svg fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
     <use href="/icons.svg#icon-view-list-filled"></use>
   </svg>
-)
+);
 
 export const StarIcon = () => (
   <svg fill="none" height="16" viewBox="0 0 16 16" width="16" xmlns="http://www.w3.org/2000/svg">
     <use href="/icons.svg#icon-star-active"></use>
   </svg>
-)
+);
 
 export const StarEmptyIcon = () => (
   <svg fill="none" height="16" viewBox="0 0 16 16" width="16" xmlns="http://www.w3.org/2000/svg">
     <use href="/icons.svg#icon-star"></use>
   </svg>
-)
+);
 
 export const ChevronDownIcon = () => (
   <svg fill="none" height="16" viewBox="0 0 16 16" width="16" xmlns="http://www.w3.org/2000/svg">
     <use href="/icons.svg#chevron-down"></use>
   </svg>
-  )
+);
+
 export const FBIcon = () => (
   <svg fill="none" height="16" viewBox="0 0 16 16" width="16" xmlns="http://www.w3.org/2000/svg">
     <use href="/icons.svg#icon-fb"></use>
   </svg>
-)
+);
 
 export const TwitterIcon = () => (
   <svg fill="none" height="16" viewBox="0 0 16 16" width="16" xmlns="http://www.w3.org/2000/svg">
     <use href="/icons.svg#icon-twitter"></use>
   </svg>
-)
+);
 
 export const IGIcon = () => (
   <svg fill="none" height="16" viewBox="0 0 16 16" width="16" xmlns="http://www.w3.org/2000/svg">
     <use href="/icons.svg#icon-instagram"></use>
   </svg>
-)
+);
