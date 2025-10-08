51d97b8e4e214a3c65428fe6eabcf9aa2014f8c8
wasyted <mataiasa.d@gmail.com>
2025-10-08 14:27:20 -0300

    feat(HEK#22): implementa enums para las tabs

--- src/components/ProductDetails/ProductDescriptionTabs.tsx
+++ src/components/ProductDetails/ProductDescriptionTabs.tsx
@@ -6,31 +6,32 @@ import DescriptionTab from './tabs/DescriptionTab';
 import AdditionalInfoTab from './tabs/AdditionalInfoTab';
 import ReviewsTab from './tabs/ReviewsTab';
 import VideoTab from './tabs/VideoTab';
+import { ProductDetailTab } from '@/types/Tabs';
 
 export default function ProductDescriptionTabs({ product }: { product: ProductData }) {
-  const [activeTab, setActiveTab] = useState('description');
+  const [activeTab, setActiveTab] = useState<ProductDetailTab>(ProductDetailTab.Description);
 
   return (
     <TabsContainer>
       <TabList>
-        <TabButton active={activeTab === 'description'} onClick={() => setActiveTab('description')}>
+        <TabButton active={activeTab === ProductDetailTab.Description} onClick={() => setActiveTab(ProductDetailTab.Description)}>
           Description
         </TabButton>
-        <TabButton active={activeTab === 'additionalInfo'} onClick={() => setActiveTab('additionalInfo')}>
+        <TabButton active={activeTab === ProductDetailTab.AdditionalInfo} onClick={() => setActiveTab(ProductDetailTab.AdditionalInfo)}>
           Additional Info
         </TabButton>
-        <TabButton active={activeTab === 'reviews'} onClick={() => setActiveTab('reviews')}>
+        <TabButton active={activeTab === ProductDetailTab.Reviews} onClick={() => setActiveTab(ProductDetailTab.Reviews)}>
           Reviews
         </TabButton>
-        <TabButton active={activeTab === 'video'} onClick={() => setActiveTab('video')}>
+        <TabButton active={activeTab === ProductDetailTab.Video} onClick={() => setActiveTab(ProductDetailTab.Video)}>
           Video
         </TabButton>
       </TabList>
       <TabContent>
-        {activeTab === 'description' && <DescriptionTab product={product} />}
-        {activeTab === 'additionalInfo' && <AdditionalInfoTab product={product} />}
-        {activeTab === 'reviews' && <ReviewsTab product={product} />}
-        {activeTab === 'video' && <VideoTab product={product} />}
+        {activeTab === ProductDetailTab.Description && <DescriptionTab product={product} />}
+        {activeTab === ProductDetailTab.AdditionalInfo && <AdditionalInfoTab product={product} />}
+        {activeTab === ProductDetailTab.Reviews && <ReviewsTab product={product} />}
+        {activeTab === ProductDetailTab.Video && <VideoTab product={product} />}
       </TabContent>
     </TabsContainer>
   );
